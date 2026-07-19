9102667e2115150837f71db28c3501f51781b90b
Rishav Raj <rishavraj9755@gmail.com>
2026-07-19 22:05:12 +0530

    more about array

---
+++ CODE 1/BASICS 02/array_02.js
@@ -0,0 +1,22 @@
+const a=[1,2,3]
+const b=[5,6,7];
+b.push(a);
+console.log(b);
+// a.concat(b);
+const c=a.concat(b);
+console.log(c);
+
+//spread easy method to add two different arrays
+// like/
+const z=[...a,...b]
+console.log(z);
+ const p=z.flat(1);
+ //mast hai flat usually infinity de do ho jata hai
+
+ console.log(p);
+
+ console.log(Array.isArray("rishav"));
+ console.log(Array.from("rishav"));
+ console.log(Array.from({name:"rishav"}));
+ 
+
