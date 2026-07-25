83298057c240143f37a163e9d51e9698172d209d
Rishav Raj <rishavraj9755@gmail.com>
2026-07-25 22:10:33 +0530

    Completed arrow fxn

--- CODE 1/BASICS 03/This and arrow fxn.js
+++ CODE 1/BASICS 03/This and arrow fxn.js
@@ -3,8 +3,46 @@ const user={
     price:999,
     welcomeMessage:function()
     {
-        console.log(`${},welcome to website`);
+        console.log(`${this.username},welcome to the website.`);
+        console.log(user);
         
+
         
     }
+
 }
+// user.welcomeMessage()
+// user.username="SAM"
+// user.welcomeMessage()
+console.log(this);
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// function chai(){
+    // let username="hi"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// function me direct this ni chalta
+// ----------------------------------------
+// chai=function (){
+//     let username="hi"
+//     console.log(this.username);
+    
+// }
+// chai()
+
+// const chai=()=>{
+//     let username="hi"
+//     console.log(this);
+    
+// }
+// chai()
+const addtwo=(num1,num2)=>(num1+num2)//isko raact me use karenge
+// const addtwo=(num1,num2)=>
+// {
+//     return num1+num2
+// }
+console.log(addtwo(2,4));
+ 
+
