49a6ddeb7946513f492041d60c83d2e62f04dcf4
Rishav Raj <rishavraj9755@gmail.com>
2026-07-28 17:49:51 +0530

    Control flow

---
+++ CODE 1/BASICS 03/control flow.js
@@ -0,0 +1,52 @@
+//alag alag condition ke liye alag alag time pe operate kare js like login pe alg log out pe alag
+
+//if
+// if(condition==true)//or false
+// {
+
+// }
+// //> , < <= >= == 
+
+//=== type bhi check karta hai
+/* if(2==="2")
+{
+    console.log("executed");
+    
+} */
+let marks = 56;
+
+if (0==" ") {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
+//if else just like cpp 
+// if(a==2 || b==2 && c==3)
+{
+
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++/'/'
+// switch case
+const month = 2;
+switch (month) {
+    case 1:
+
+        console.log("jan");
+        break;
+    case 2:
+        console.log("fan");
+        break;
+    default:
+        console.log("default");
+        break;
+
+
+
+}//falsy values
+//false ,0,-0,BigInt 0n "".null.,undefined ,nan
+
+// truthy values
+// "0",'false' " ".[]{},function(){}
+Object.keys().length==0
+//nullish operaTOR
