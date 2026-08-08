a3408ed894347f871ffce84ef48d4c742e484fbf
Rishav Raj <rishavraj9755@gmail.com>
2026-08-08 23:12:11 +0530

    added js

--- CODE 1/Projects/1.COLOR CHANGER/script.js
+++ CODE 1/Projects/1.COLOR CHANGER/script.js
@@ -3,4 +3,11 @@ const body = document.querySelector('body');
 
 buttons.forEach(function(button) {
     console.log(button);
+    button.addEventListener('click', funtion(e)
+{
+    console.log(e)
+    console.log(e.target)
+    
+    
+}
 });
